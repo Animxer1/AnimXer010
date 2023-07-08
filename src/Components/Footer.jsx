df44e186371818a54d1d03a86c15f5481dc5693a
Animxer1 <134288691+Animxer1@users.noreply.github.com>
2023-07-08 10:32:24 +0800

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,14 +18,14 @@ function Footer() {
   return (
     <div className="footer">
       <h1>
-        <span className="white">Anime</span>Trix
+        <span className="white">Anim</span>Xer
       </h1>
       <p className="footer-about">
-        AnimeTrix is not affiliated with or endorsed by any of the anime studios
+        AnimXer is not affiliated with or endorsed by any of the anime studios
         behind the creation of the anime presented on this site. This website is
         only an user interface presenting/linking various self-hosted files
         across the internet by other third-party providers for easy access.
-        AnimeTrix never downloads the video from any source provider, link will
+        AnimXer never downloads the video from any source provider, link will
         be returned from the response hence it is completely not subjected to
         DMCA compliant.
       </p>
